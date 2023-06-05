aa584677b434b5ee50f3bfc93d7bb6489fc43ad5
UDES03 <nin.than1121@gmail.com>
2023-06-05 12:09:34 +1000

    deploy3

--- app/__init__.py
+++ app/__init__.py
@@ -11,12 +11,13 @@ webApp = Flask(__name__)
 
 mysqlUrl = urllib.parse.urlparse(os.environ['MYSQL_URL'])
 
-webApp.config['MYSQL_DATABASE_HOST'] = mysqlUrl.hostname
-webApp.config['MYSQL_DATABASE_PORT'] = mysqlUrl.port
-webApp.config['MYSQL_DATABASE_USER'] = mysqlUrl.username
-webApp.config['MYSQL_DATABASE_PASSWORD'] = mysqlUrl.password
-webApp.config['MYSQL_DATABASE_DB'] = mysqlUrl.path.lstrip('/')
+webApp.config['MYSQL_HOST'] = mysqlUrl.hostname
+webApp.config['MYSQL_PORT'] = mysqlUrl.port
+webApp.config['MYSQL_USER'] = mysqlUrl.username
+webApp.config['MYSQL_PASSWORD'] = mysqlUrl.password
+webApp.config['MYSQL_DATABASE'] = mysqlUrl.path.lstrip('/')
 
 mysql = MySQL(webApp)
+mysql.init_app(webApp)
 
 import app.views
